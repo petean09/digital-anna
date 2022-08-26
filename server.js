2bd7adcb615a4f69ef52ca593ff140ad0cc336f8
petean09 <petean09@luther.edu>
2022-08-26 14:05:59 -0500

    updates to deploy heroku

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const path = require("path");
-const express = require("express");
+//Install express server
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/angular-build'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
