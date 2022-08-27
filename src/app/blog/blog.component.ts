0bc0fb70a6dd2e70dd83d8a674959382f104aeda
petean09 <petean09@luther.edu>
2022-08-27 18:49:13 -0500

    missed pushing some changes

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ContentfulService } from '../services/contentful.service';
 
 @Component({
   selector: 'app-blog',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  constructor( private contentfulService : ContentfulService) { }
+
+  // creates the variable that will hold the observable from contentfulService
+  blogPosts$ : Observable<any> | undefined;
 
   ngOnInit(): void {
+    // retrieves all the blog posts from the contentful service. This is an observable
+    this.blogPosts$ = this.contentfulService.getAllEntries();
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  spaceId: 'ys0go1t5lqf1',
+  accessToken: 'wAk_qis2o_D-m3K-NeD7RsQkPQEuR-0EN3NUlKegr2c'
 };
 
 /*
