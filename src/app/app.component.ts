4fffe8c906ef8b5d24fc74f319af47294cac2153
petean09 <petean09@luther.edu>
2022-09-06 21:23:59 -0500

    added filter to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,19 @@ export class AppComponent {
   constructor() { }
   menu_icon_variable: boolean = false;
   menuVariable: boolean = false;
+  navbarContainer: boolean = false;
   ngOnInit(): void {
   }
   openMenu() {
     this.menuVariable =! this.menuVariable;
     this.menu_icon_variable =! this.menu_icon_variable;
+    this.navbarContainer =! this.navbarContainer;
   }
 
   closeMenu() {
     this.menuVariable =! this.menuVariable;
     this.menu_icon_variable =! this.menu_icon_variable;
+    this.navbarContainer =! this.navbarContainer;
   }
 }
 
