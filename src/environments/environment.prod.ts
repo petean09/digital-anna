d2dcf985e2167357cf150bc160d5224d11fc8031
petean09 <petean09@luther.edu>
2022-09-09 20:00:27 -0500

    added back api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  CONTENTFUL_SPACE_ID: 'undefined',
-  CONTENTFUL_ACCESS_TOKEN: 'undefined'
+  CONTENTFUL_SPACE_ID: 'ys0go1t5lqf1',
+  CONTENTFUL_ACCESS_TOKEN: 'ucYZGsIwdjF02Y4kh3BH6yfkn5NXbRKw_6Qqe3E52ZQ'
 };
