edca94b0168876c8c415c7707cc27f1e7c6b6196
petean09 <petean09@luther.edu>
2022-09-09 19:19:59 -0500

    reinstalled node modules

--- src/app/services/contentful.service.ts
+++ src/app/services/contentful.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { provideRoutes } from '@angular/router';
 import { createClient, Entry } from 'contentful';
 import { from } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
