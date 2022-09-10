45b7f009ac22c758a052914609eb57979db44e53
petean09 <petean09@luther.edu>
2022-09-09 19:21:11 -0500

    trying to get vercel to work

--- src/app/services/contentful.service.ts
+++ src/app/services/contentful.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { provideRoutes } from '@angular/router';
 import { createClient, Entry } from 'contentful';
 import { from } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
