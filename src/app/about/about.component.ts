70bdb2d48954deadb1e29291b7c759690646ea59
petean09 <petean09@luther.edu>
2022-09-25 15:40:36 -0500

    about pics added

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ContentfulService } from '../services/contentful.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor( private contentfulService : ContentfulService) { }
+
+  // creates the variable that will hold the observable from contentfulService
+  aboutPics$ : Observable<any> | undefined;
 
   ngOnInit(): void {
+    // retrieves all the about pics from the contentful service. This is an observable
+    this.aboutPics$ = this.contentfulService.getAllEntries();
+
   }
 
 }
